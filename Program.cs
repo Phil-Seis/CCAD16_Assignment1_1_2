cef8999a2016017d9a33e9b1401574612620dcbf
Philip Seisman <Philip.seisman@gmail.com>
2024-11-05 10:09:47 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace CCAD16_Assignment1_1_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a simple console application and declare basic datatypes like int, float, decimal, double and print their min and max values.
+            int x = 5;
+            float y = 44;
+            decimal z = 33.4m;
+            double a = 16.55;
+
+            Console.WriteLine(Math.Max(x, z));
+            Console.WriteLine(Math.Min(y, a));
+        }
+    }
+}
